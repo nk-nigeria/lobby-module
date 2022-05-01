21855b871ba5197309dae2ed506fb337955d01b2
baohavan <baohavan@gmail.com>
2022-05-01 16:15:13 +0700

    user: link username, change password

--- pkg/nakama/nakama.go
+++
@@ -1,9 +0,0 @@
-package nakama
-
-import "regexp"
-
-var (
-	invalidUsernameRegex = regexp.MustCompilePOSIX("([[:cntrl:]]|[[\t\n\r\f\v]])+")
-	invalidCharsRegex    = regexp.MustCompilePOSIX("([[:cntrl:]]|[[:space:]])+")
-	emailRegex           = regexp.MustCompile("^.+@.+\\..+$")
-)
