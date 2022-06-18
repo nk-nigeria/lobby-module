5e484c578d171b36cb71a70634331a63ee97e33c
Nguyễn Văn Phước <phuocpr1998@gmail.com>
2022-06-18 23:58:09 +0700

    update rpc add notification

--- api/notification_admin.go
+++ api/notification_admin.go
@@ -12,13 +12,22 @@ import (
 
 func RpcAddNotification(marshaler *protojson.MarshalOptions, unmarshaler *protojson.UnmarshalOptions) func(context.Context, runtime.Logger, *sql.DB, runtime.NakamaModule, string) (string, error) {
 	return func(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-		request := &pb.Notification{}
-		if err := unmarshaler.Unmarshal([]byte(payload), request); err != nil {
+		request := &pb.AddNotificationRequest{}
+		if err := unmarshaler.Unmarshal([]byte(payload), request); err != nil || len(request.RecipientIds) == 0 {
 			logger.Error("unmarshal notification error %v", err)
 			return "", presenter.ErrUnmarshal
 		}
 		request.SenderId = ""
-		err := cgbdb.AddNotification(ctx, logger, db, nk, request)
+		// TODO get list user by group
+		notification := &pb.Notification{
+			RecipientId: request.RecipientIds[0],
+			Type:        request.Type,
+			Title:       request.Title,
+			Content:     request.Content,
+			SenderId:    "",
+			Read:        false,
+		}
+		err := cgbdb.AddNotification(ctx, logger, db, nk, notification)
 		if err != nil {
 			logger.Error("Add notification user %s, error: %s", request.SenderId, err.Error())
 			return "", err
