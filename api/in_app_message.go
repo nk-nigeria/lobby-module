17350f9117c088db1768d30a34fac5faa19a2c41
Nguyễn Văn Phước <phuocpr1998@gmail.com>
2022-06-30 08:57:22 +0700

    get url image of in app message

--- api/in_app_message.go
+++ api/in_app_message.go
@@ -6,7 +6,10 @@ import (
 	"github.com/ciaolink-game-platform/cgp-lobby-module/api/presenter"
 	"github.com/ciaolink-game-platform/cgp-lobby-module/cgbdb"
 	"github.com/ciaolink-game-platform/cgp-lobby-module/conf"
+	objectstorage "github.com/ciaolink-game-platform/cgp-lobby-module/object-storage"
 	pb "github.com/ciaolink-game-platform/cgp-lobby-module/proto"
+	"strings"
+	"time"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -45,7 +48,7 @@ func RpcUpdateInAppMessage(marshaler *protojson.MarshalOptions, unmarshaler *pro
 	}
 }
 
-func RpcListInAppMessage(marshaler *protojson.MarshalOptions, unmarshaler *protojson.UnmarshalOptions) func(context.Context, runtime.Logger, *sql.DB, runtime.NakamaModule, string) (string, error) {
+func RpcListInAppMessage(marshaler *protojson.MarshalOptions, unmarshaler *protojson.UnmarshalOptions, wrapper objectstorage.ObjStorage) func(context.Context, runtime.Logger, *sql.DB, runtime.NakamaModule, string) (string, error) {
 	return func(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
 		inAppMessageRequest := &pb.InAppMessageRequest{}
 		if payload != "" {
@@ -57,6 +60,25 @@ func RpcListInAppMessage(marshaler *protojson.MarshalOptions, unmarshaler *proto
 		list, err := cgbdb.GetListInAppMessage(ctx, logger, db, unmarshaler, nk, inAppMessageRequest.Limit, inAppMessageRequest.Cusor, inAppMessageRequest.Type)
 		if err != nil {
 			return "", err
+		}
+		for _, inAppMessage := range list.InAppMessages {
+			if inAppMessage.Data != nil && inAppMessage.Data.Params != nil {
+				if lstImageStr, ok := inAppMessage.Data.Params["images"]; ok {
+					lstImage := strings.Split(lstImageStr, ";")
+					lstImageTmp := make([]string, 0)
+					for _, img := range lstImage {
+						sepIdx := strings.Index(img, "/")
+						bucketName := img[:sepIdx]
+						fileName := img[sepIdx+1:]
+						if url, err := wrapper.PresignGetObject(bucketName, fileName, 24*time.Hour, nil); err == nil {
+							lstImageTmp = append(lstImageTmp, url)
+						} else {
+							logger.Error("PresignGetObject %s error %s", img, err.Error())
+						}
+					}
+					inAppMessage.Data.Params["images"] = strings.Join(lstImageTmp, ";")
+				}
+			}
 		}
 		listInAppMessageStr, _ := conf.MarshalerDefault.Marshal(list)
 		return string(listInAppMessageStr), nil
