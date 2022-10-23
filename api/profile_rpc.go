df493fb5eb622afe6b495489a0fe36b0c9085774
DANGQUOCSON <dangquocson1995@gmail.com>
2022-10-23 21:23:54 +0700

    [update]: add field match id in profile

--- api/profile_rpc.go
+++ api/profile_rpc.go
@@ -206,6 +206,7 @@ func GetProfileUser(ctx context.Context, nk runtime.NakamaModule, userID string,
 		VipLevel:           entity.ToInt64(metadata["vip_level"], DefaultLevel),
 		LastOnlineTimeUnix: entity.ToInt64(metadata["last_online_time_unix"], 0),
 		CreateTimeUnix:     user.GetCreateTime().Seconds,
+		PlayingMatch:       entity.InterfaceToString(metadata["on_playing_in_match"]),
 		// LangAvailables:     []string{"en", "phi"},
 	}
 	profile.LangAvailables = append(profile.LangAvailables,
